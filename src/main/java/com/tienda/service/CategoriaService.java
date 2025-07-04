e57e4d942541ae11d52ee3960bea63eef3684bdb
StevenSGS <108242569+StevenSGS@users.noreply.github.com>
2025-07-03 18:11:42 -0600

    Commit

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -6,4 +6,7 @@ import java.util.List;
 public interface CategoriaService {
     //asigna el metodo para despues implementarlo en el serviceImpl (Read)
     public List<Categoria> getCategorias(boolean activos);
+    public Categoria getCategoria(Categoria categoria);
+    public void save(Categoria categoria);
+    public void delete(Categoria categoria);
 }
