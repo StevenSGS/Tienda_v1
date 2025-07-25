71bc150b0b05a2405fdf164f51e1482ec86cc219
StevenSGS <108242569+StevenSGS@users.noreply.github.com>
2025-07-24 20:52:10 -0600

    Commit

--- src/main/java/com/tienda/service/ProductoService.java
+++ src/main/java/com/tienda/service/ProductoService.java
@@ -2,6 +2,7 @@ package com.tienda.service;
 
 import com.tienda.domain.Producto; 
 import java.util.List; 
+import org.springframework.data.repository.query.Param;
 
 public interface ProductoService {
     //asigna el metodo para despues implementarlo en el serviceImpl (Read)
@@ -9,4 +10,8 @@ public interface ProductoService {
     public Producto getProducto(Producto producto);
     public void save(Producto producto);
     public void delete(Producto producto);
+    
+    public List<Producto> findByPrecioBetweenOrderByDescripcion(double precioInf, double precioSup);
+    public List<Producto> metodoJPQL(@Param("precioInf") double precioInf, @Param("precioSup") double precioSup);
+    public List<Producto> metodoNativo(@Param("precioInf") double precioInf, @Param("precioSup") double precioSup);
 }
